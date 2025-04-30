42b41895249c54d0b7e6c40c9036774b19de652d
Yuu <152776955+yuukun123@users.noreply.github.com>
2025-04-30 16:05:48 +0700

    update

--- ToDoList/todoItem.py
+++ ToDoList/todoItem.py
@@ -1,7 +1,8 @@
 # Class đại diện cho 1 nhiệm vụ
 class TodoItem:
-    def __init__(self, title, completed=False):
+    def __init__(self, title, description="", completed=False):
         self.title = title
+        self.description = description
         self.completed = completed
 
     def to_dict(self):
